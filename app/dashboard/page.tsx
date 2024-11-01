045a0cff79720a9dc72cde286f76be7b11d2da05
Add852 <xadd852x@gmail.com>
2024-11-01 19:07:28 +0800

    inayos usestate and missing key error

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
 import WalletBar from '../components/WalletBar';
 import user from '../_data/userDataSample.json'
 
