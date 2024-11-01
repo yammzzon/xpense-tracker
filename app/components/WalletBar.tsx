0766d6bea8717088ee4ca431f27d79027846795b
Add852 <xadd852x@gmail.com>
2024-11-01 17:44:30 +0800

    user data json sample

--- app/components/WalletBar.tsx
+++ app/components/WalletBar.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import user from '../_data/userDataSample.json'
 
 export default function WalletBar() {
     return (
         <div className='fixed bottom-0 bg-three w-screen h-8'>
             <div className='flex flex-row z-50 space-x-2 pl-5'>
-                <div className='rounded-b-2xl bg-one w-36 text-center text-five'>Wallet 1</div>
-                <div className='rounded-b-2xl bg-two w-36 text-center text-four'>Wallet 2</div>
+                {user.wallets.map((wallet,i) => (
+                    <button
+                        className={`rounded-b-2xl w-36 text-center text-four 
+                        ${i===user.selectedWallet ? 'bg-one' : 'bg-two'}`
+                    }>{wallet.walletName}</button>
+                ))}
                 <div className='rounded-b-2xl bg-two w-12 text-center text-four'>+</div>
             </div>
         </div>
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import WalletBar from '../components/WalletBar';
+import user from '../_data/userDataSample.json'
 
 export default function Dashboard() {
+
+    /* TO DO:
+        0.
+        1.default get parameter for dashboard
+            www.link/dashboard?walletid=0
+        2.how to submit
+        
+        or trash all that and just completely use server side.
+
+    */
+    const currWallet = user.wallets[user.selectedWallet];
+
     return (
-        <main className='pt-6 h-screen'>
+        <main className='pt-6 h-screen bg-one'>
             {/* Profile + Balance + Add + Menu */}
-            <div className='bg-one w-full fixed flex justify-center items-center px-5 md:px-10 lg:px-20 space-x-5 h-20 py-2'>
+            <div className='bg-one border border-three w-full fixed flex justify-center items-center px-5 md:px-10 lg:px-20 space-x-5 h-20 py-2'>
                 <img src="/happy.png" alt='user profile picture' className='rounded-full h-full' />
                 <div className='flex bg-two rounded-xl w-full h-full justify-between items-center px-5'>
-                    <p className='text-four text-2xl font-bold'>$5,000.00</p>
+                    <p className='text-four text-2xl font-bold'>${currWallet.walletBalance}</p>
                     <button className='bg-three rounded-xl h-1/2 px-5 text-one'>Add</button>
                 </div>
                 <img src="/more.png" alt="menu bar" className='h-full' />
