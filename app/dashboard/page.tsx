92ab3e6e79450c67a5e8fb6712f9d9f82431586d
knmnltzy <knmnltzy@gmail.com>
2024-11-01 22:19:42 +0800

    fix img

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -20,12 +20,12 @@ export default function Dashboard() {
         <main className='pt-6 h-screen bg-one'>
             {/* Profile + Balance + Add + Menu */}
             <div className='bg-one border border-three w-full fixed flex justify-center items-center px-5 md:px-10 lg:px-20 space-x-5 h-20 py-2'>
-                <Image src="/happy.png" alt='user profile picture' className='rounded-full h-full' />
+                <Image src="/happy.png" alt='user profile picture' className='rounded-full h-full' height={0} width={100} />
                 <div className='flex bg-two rounded-xl w-full h-full justify-between items-center px-5'>
                     <p className='text-four text-2xl font-bold'>${currWallet.walletBalance}</p>
                     <button className='bg-three rounded-xl h-1/2 px-5 text-one'>Add</button>
                 </div>
-                <Image src="/more.png" alt="menu bar" className='h-full' />
+                <Image src="/more.png" alt="menu bar" className='h-full' height={0} width={100} />
             </div>
             {/* PANELS */}
             <div className='bg-one overflow-y-scroll grid gap-5 sm:grid-cols-2 lg:grid-cols-3 md:px-10 lg:px-20 pt-24 pb-12 px-5 '>
